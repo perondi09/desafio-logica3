2923955a7f8f811e4a1e73f8a9ae132049118006
perondi <guilhermeperondi09@gmail.com>
2024-08-19 14:16:33 -0300

    primeiro commit

---
+++ classes.js
@@ -0,0 +1,27 @@
+class Hero {
+    constructor(name, age, type) {
+        this.name = name;
+        this.age = age;
+
+        this.types = ["mago", "guerreiro", "monge", "ninja"];
+        this.attacks = ["magia", "espada", "artes marciais", "shuriken"]
+        this.type = type % this.types.length;
+    }
+    getType() {
+        return this.types[this.type];
+    }
+    getAttack() {
+        return this.attacks[this.type];
+    }
+    attack() {
+        console.log(`O ${this.getType()} atacou usando ${this.getAttack()}`);
+    }
+    info() {
+        console.log(`Nome: ${this.name}, idade: ${this.age}, Tipo: ${this.getType()}`);
+    }
+}
+
+for(let i = 0; i < 4; i++) {
+    let perondi = new Hero("perondi", 0, i);
+    perondi.attack();
+}
